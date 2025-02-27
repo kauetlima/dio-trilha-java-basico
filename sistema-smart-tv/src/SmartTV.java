b3409d10b21562bcc25ecbc2dc5df42a4f9a9dec
kauetlima <kauet123@live.com>
2025-02-27 18:10:14 -0300

    Calculadora de soma

--- sistema-smart-tv/src/SmartTV.java
+++ sistema-smart-tv/src/SmartTV.java
@@ -8,26 +8,20 @@ public class SmartTV {
     public void mudarCanal (int canalNovo) {
         canal = canalNovo;
     }
-
     public void aumentarCanal(){
         canal++;
     }
-
     public void diminuirCanal(){
         canal--;
     }
-
-
     public void aumentarVolume(){
         volume++;
         System.out.println("Aumentando volume atual : " + volume);
     }
-    
     public void diminuirVolume(){
         volume--;
         System.out.println("Diminuindo volume atual : " + volume);
     }
-
     public void ligar(){
     ligada = true;
     }
